6efea9f40f6629f89e1c5f792363fa9eb307a4bc
Yevhenii <fonjeekay@gmail.com>
2025-06-01 15:16:54 +0300

    routing change

--- routing.go
+++ routing.go
@@ -11,14 +11,14 @@ func (srv Server) Routing() *mux.Router {
 
 	mx := mux.NewRouter()
 	mx.HandleFunc("/", handlers.IndexHandler).Methods("GET")
-	mx.HandleFunc("/registration", handlers.RegistrationHandler).Methods("POST").Queries("email", "{email}", "password", "{password}", "name", "{name}")
-	mx.HandleFunc("/login", handlers.LoginHandler).Methods("POST").Queries("email", "{email}", "password", "{password}")
+	mx.HandleFunc("/api/registration", handlers.RegistrationHandler).Methods("POST").Queries("email", "{email}", "password", "{password}", "name", "{name}")
+	mx.HandleFunc("/api/login", handlers.LoginHandler).Methods("POST").Queries("email", "{email}", "password", "{password}")
 
 	// CRUD for notes
-	mx.HandleFunc("/create/note", handlers.CreateNoteHandler).Methods("POST").Queries("note", "{note}", "token", "{token}")
-	mx.HandleFunc("/read/notes", handlers.ReadNotesHandler).Methods("GET").Queries("page", "{page}", "token", "{token}")
-	mx.HandleFunc("/update/note", handlers.UpdateNotesHandler).Methods("PUT").Queries("id", "{id}", "note", "{note}", "token", "{token}")
-	mx.HandleFunc("/delete/note", handlers.DeleteNoteHandler).Methods("DELETE").Queries("id", "{id}", "token", "{token}")
+	mx.HandleFunc("/api/create/note", handlers.CreateNoteHandler).Methods("POST").Queries("note", "{note}", "token", "{token}")
+	mx.HandleFunc("/api/read/notes", handlers.ReadNotesHandler).Methods("GET").Queries("page", "{page}", "token", "{token}")
+	mx.HandleFunc("/api/update/note", handlers.UpdateNotesHandler).Methods("PUT").Queries("id", "{id}", "note", "{note}", "token", "{token}")
+	mx.HandleFunc("/api/delete/note", handlers.DeleteNoteHandler).Methods("DELETE").Queries("id", "{id}", "token", "{token}")
 
 	return mx
 }
